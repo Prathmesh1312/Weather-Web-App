4efd4f73e56e356cb732ecc4a8490cda5f8f7a50
Prathmesh <tokeprathmesh54321@gmail.com>
2024-10-01 11:13:23 +0530

    CHECK .

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
     },
+  },
+  server: {
+    port: process.env.PORT,
+  },
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
   },
 });
