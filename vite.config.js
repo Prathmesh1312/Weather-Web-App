c5cf2739af5813cc792f251e550023672618d528
Prathmesh1312 <107990814+Prathmesh1312@users.noreply.github.com>
2024-10-01 14:51:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vercel()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
